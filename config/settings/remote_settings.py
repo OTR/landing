023bf089412fa715e499588613a76b6b553d9514
OTR <49165533+OTR@users.noreply.github.com>
2024-11-26 16:17:07 +0600

    [CHORE] Add local docker-compose cofiguration

---
+++ config/settings/remote_settings.py
@@ -0,0 +1,14 @@
+import os
+
+from django.core.management.utils import get_random_secret_key
+from dotenv import load_dotenv
+
+from config.settings.common_settings import *
+
+PATH_TO_ENVIRONMENT_VARIABLES = Path(__file__).parent.parent.parent / "prod.env"
+load_dotenv(PATH_TO_ENVIRONMENT_VARIABLES)
+
+DEBUG = False
+SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
+TIME_ZONE = "Etc/GMT-3"
--- config/settings/test_settings.py
+++ config/settings/test_settings.py
@@ -1,5 +1,3 @@
-import os
-
 from dotenv import load_dotenv
 
 from config.settings.common_settings import *
@@ -7,6 +5,6 @@ from config.settings.common_settings import *
 PATH_TO_ENVIRONMENT_VARIABLES = Path(__file__).parent.parent.parent / "prod.env"
 load_dotenv(PATH_TO_ENVIRONMENT_VARIABLES)
 
-DEBUG = False
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
+DEBUG = True
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "0.0.0.0"]
 TIME_ZONE = "Etc/GMT-3"
