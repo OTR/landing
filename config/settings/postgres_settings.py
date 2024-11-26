cb75b63e3bfa1a356e9ecb075191c34aefdad82e
OTR <49165533+OTR@users.noreply.github.com>
2024-11-26 15:37:25 +0600

    [FIX] Fill up Django allowed hosts

--- config/settings/postgres_settings.py
+++ config/settings/postgres_settings.py
@@ -1,6 +1,12 @@
+import os
+
 from dotenv import load_dotenv
 
 from config.settings.common_settings import *
 
 PATH_TO_ENVIRONMENT_VARIABLES = Path(__file__).parent.parent.parent / "prod.env"
 load_dotenv(PATH_TO_ENVIRONMENT_VARIABLES)
+
+DEBUG = False
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
+TIME_ZONE = "Etc/GMT-3"
