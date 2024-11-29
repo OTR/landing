f7e40e5a919944cab47c0609a013eb03f0b228d8
Andrew <49165533+OTR@users.noreply.github.com>
2024-11-29 17:59:44 +0600

    [CHORE] Remove static files from Django repository

--- landing_app/views/index.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-from django.http import HttpResponse
-
-def serve_html_file(request):
-    path = str(settings.BASE_DIR / "static" / "index.html")
-    with open(path, "r", encoding="utf-8") as f:
-        html_content = f.read()
-    return HttpResponse(html_content, content_type="text/html")
